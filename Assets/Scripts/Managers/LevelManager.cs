f0771653ef13940d40cc1349384a72960fd0e4ea
VladGaranovskyi <garanovskiyvlad2005@gmail.com>
2024-02-17 11:36:28 -0400

    + Unity Ads

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
 {
     [SerializeField] private GameObject _menuUI;
     [SerializeField] private GameObject _gameUI;
@@ -11,6 +12,7 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private Text _earnedCoinsText;
     [SerializeField] private PlayerController _playerController;
     [SerializeField] private GameObject _playerDoll;
+    [SerializeField] private string _androidAdUnitId = "Interstitial_Android";
     public static LevelManager instance;
 
     public float Multiplier { private get; set; }
@@ -52,11 +54,16 @@ public class LevelManager : MonoBehaviour
 
     public void EndGame()
     {
+        LoadAd();
+    }
+
+    private void GameEnd()
+    {
         PlayerPrefs.SetInt("Coins", PlayerPrefs.GetInt("Coins") + Mathf.RoundToInt(_playerController.Coins * Multiplier));
         if (PlayerPrefs.HasKey("Level"))
         {
             PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level") + 1);
-            if(PlayerPrefs.GetInt("Level") > 2)
+            if (PlayerPrefs.GetInt("Level") > 2)
             {
                 PlayerPrefs.DeleteKey("Level Config");
             }
@@ -66,5 +73,44 @@ public class LevelManager : MonoBehaviour
             PlayerPrefs.SetInt("Level", 2);
         }
         SceneManager.LoadScene("Loading");
+    }
+
+    private void LoadAd()
+    {
+        Debug.Log("Loading Ad: " + _androidAdUnitId);
+        Advertisement.Load(_androidAdUnitId, this);
+    }
+
+    // Show the loaded content in the Ad Unit:
+    public void ShowAd()
+    {
+        // Note that if the ad content wasn't previously loaded, this method will fail
+        Debug.Log("Showing Ad: " + _androidAdUnitId);
+        Advertisement.Show(_androidAdUnitId, this);
+    }
+
+    // Implement Load Listener and Show Listener interface methods: 
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        ShowAd();
+    }
+
+    public void OnUnityAdsFailedToLoad(string _adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {_adUnitId} - {error.ToString()} - {message}");
+        GameEnd();
+    }
+
+    public void OnUnityAdsShowFailure(string _adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {_adUnitId}: {error.ToString()} - {message}");
+        GameEnd();
+    }
+
+    public void OnUnityAdsShowStart(string _adUnitId) { }
+    public void OnUnityAdsShowClick(string _adUnitId) { }
+    public void OnUnityAdsShowComplete(string _adUnitId, UnityAdsShowCompletionState showCompletionState) 
+    {
+        GameEnd();
     }
 }
